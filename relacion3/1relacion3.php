43144ab13727fba26457913118de96702f269028
Felipe GonMa <fgonmar445@g.educaand.es>
2025-11-04 18:07:47 +0100

    'Relaicon3'

--- relacion3/1relacion3.php
+++ relacion3/1relacion3.php
@@ -9,34 +9,43 @@
 
 <body>
   <h4>Comprobar si el numero es primo</h4>
-  <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
+  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
     <label for="num1">Introduce un número:</label>
     <input type="number" class="form-control" name="num1" id="num1" required placeholder="Introduce un numero positivo">
-    <button type="submit" class="btn btn-success">Enviar</button>
+    <input type="submit" class="btn btn-success" value="Enviar">
     <?php
 
-    function esPrimo($num) {
-    
-        if ($num < 2 || $num != intval($num)) {
+    function esPrimo($num): bool
+    {
+
+      if ($num < 2 || $num != intval($num)) {
         return false;
+      }
+
+      for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0)
+          return false;
+      }
+
+      return true;
     }
 
-    for ($i = 2; $i <= sqrt($num); $i++) {
-      if ($num % $i == 0)
-        return false;
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['num1'])) {
+      $numero = $_GET['num1'];
+      if (esPrimo($numero)) {
+        echo "<div class='alert pt-5 alert-success mt-3'>El número $numero es primo.</div>";
+      } else {
+        echo "<div class='alert pt-3 alert-danger mt-3'>El número $numero no es primo.</div>";
+      }
     }
-
-    return true;
-  }
-
-  if (isset($_GET['num1'])) {
-    $numero = $_GET['num1'];
-    if (esPrimo($numero)) {
-      echo "<div class='alert pt-5 alert-success mt-3'>El número $numero es primo.</div>";
-    } else {
-      echo "<div class='alert pt-3 alert-danger mt-3'>El número $numero no es primo.</div>";
+    echo "<p><strong>Números primos entre 1 y $numero:</strong></p>";
+    echo "<ul class='list-group'>";
+    for ($i = 1; $i <= $numero; $i++) {
+      if (esPrimo($i)) {
+        echo "<li class='list-group-item'>$i</li>";
+      }
     }
-  }
+    echo "</ul>";
     ?>
 </body>
 
