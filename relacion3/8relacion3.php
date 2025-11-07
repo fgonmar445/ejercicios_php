cdeaf90a0adc290bb8f7b40ed6fd6ec689264316
Felipe GonMa <fgonmar445@g.educaand.es>
2025-11-07 18:16:16 +0100

    '8'

---
+++ relacion3/8relacion3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 8 Relacion 3</title>
+</head>
+
+<body>
+    <form>
+        <br><br>
+        <label for="texto">Introduce el texto</label>
+        <input type="text" id="texto" name="texto"><br><br>
+        <input type="radio" name="tipoTexto" id="tipoTextoMayus">
+        <label for="tipoTextoMayus">TEXTO EN MAYUSCULAS</label>
+        <input type="radio" name="tipoTexto" id="tipoTextoMinus">
+        <label for="tipoTextoMinus">texto en minusculas</label>
+
+    </form>
+</body>
+
+</html>
