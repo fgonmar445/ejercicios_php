8e446cee2a4a80f5306023a38bb370c99b497621
W10 <fgonmar445@g.educaand.es>
2025-11-11 13:28:30 +0100

    'Ej8Rel3'

--- relacion3/8relacion3.php
+++ relacion3/8relacion3.php
@@ -8,16 +8,31 @@
 </head>
 
 <body>
-    <form>
+    <form method="post" action=" <?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
         <br><br>
         <label for="texto">Introduce el texto</label>
         <input type="text" id="texto" name="texto"><br><br>
-        <input type="radio" name="tipoTexto" id="tipoTextoMayus">
+        <input type="radio" name="tipoTexto" id="textoMayus" value="mayus">
         <label for="tipoTextoMayus">TEXTO EN MAYUSCULAS</label>
-        <input type="radio" name="tipoTexto" id="tipoTextoMinus">
-        <label for="tipoTextoMinus">texto en minusculas</label>
+        <input type="radio" name="tipoTexto" id="textoMinus" value="minus">
+        <label for="tipoTextoMinus">texto en minusculas</label><br>
+        <input type="submit" value="Enviar">
 
     </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['texto']) && isset($_POST['tipoTexto'])) {
+        $texto = htmlspecialchars($_POST['texto']);
+        $tipo = $_POST['tipoTexto'];
+
+        if ($tipo == 'minus') {
+            $resul = strtolower($texto);
+            echo "El texto en minus es: $resul";
+        }
+    }
+
+
+    ?>
 </body>
 
 </html>
